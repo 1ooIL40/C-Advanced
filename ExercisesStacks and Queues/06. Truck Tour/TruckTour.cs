759cf88e84bfb9643a6a7a1be1f64eeb102d5248
Stoil Valkov <valkovstoil@abv.bg>
2018-05-10 14:43:05 +0300

    Truck and Tour TODO

---
+++ ExercisesStacks and Queues/06. Truck Tour/TruckTour.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Truck_Tour
+{
+    class TruckTour
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var petrolAmount = input[0];
+                var distance = input[1];
+
+                if(petrolAmount >= distance)
+                {
+                    queue.Enqueue(i);
+                }
+
+            }
+            Console.WriteLine(queue.Peek());
+        }
+    }
+}
