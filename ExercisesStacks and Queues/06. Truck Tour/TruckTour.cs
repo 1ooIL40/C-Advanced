bb8bcc24dff84cca2ff279c9e21d3f5f790bfa91
Stoil Valkov <valkovstoil@abv.bg>
2018-05-11 11:05:03 +0300

    Fixed it works 100%

--- ExercisesStacks and Queues/06. Truck Tour/TruckTour.cs
+++ ExercisesStacks and Queues/06. Truck Tour/TruckTour.cs
@@ -6,13 +6,11 @@ namespace _06._Truck_Tour
 {
     class TruckTour
     {
-        //TODO Fix Time LIMIT on Test 2
         static void Main(string[] args)
         {
             var n = int.Parse(Console.ReadLine());
 
             var amountOfFlue = new Queue<int>();
-            var distance = new Queue<int>();
 
             for (int i = 0; i < n; i++)
             {
@@ -20,23 +18,20 @@ namespace _06._Truck_Tour
                     .Select(int.Parse)
                     .ToArray();
 
-                amountOfFlue.Enqueue(input[0]);
-                distance.Enqueue(input[1]);
+                amountOfFlue.Enqueue(input[0] - input[1]);
             }
 
             for (int i = 0; i < n; i++)
             {
-                var workOnTheQueueFlue = new Queue<int>(amountOfFlue);
-                var workOnTheQueueDistance = new Queue<int>(distance);
+                var distance = new Queue<int>(amountOfFlue);
 
-                var flue = workOnTheQueueFlue.Dequeue() - workOnTheQueueDistance.Dequeue();
+                var flue = distance.Dequeue();
 
-                while (workOnTheQueueFlue.Count > 0)
+                while (distance.Count > 0)
                 {
                     if (flue >= 0)
                     {
-                        flue += workOnTheQueueFlue.Dequeue();
-                        flue -= workOnTheQueueDistance.Dequeue();
+                        flue += distance.Dequeue();
                     }
                     else
                     {
@@ -51,8 +46,6 @@ namespace _06._Truck_Tour
                 }
 
                 amountOfFlue.Enqueue(amountOfFlue.Dequeue());
-                distance.Enqueue(distance.Dequeue());
-
             }
         }
     }
