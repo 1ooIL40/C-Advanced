19b2453ea1326fbe6befc6f16b690cd795baf3aa
Stoil Valkov <valkovstoil@abv.bg>
2018-05-16 14:00:26 +0300

    working 100/100

---
+++ ExercisesStacks and Queues/09. Stack Fibonacci/StackFibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09._Stack_Fibonacci
+{
+    class StackFibonacci
+    {
+        static void Main(string[] args)
+        {
+            var inputNumber = int.Parse(Console.ReadLine());
+
+            var stackOfFibonacciNumbers = new Stack<long>();
+            stackOfFibonacciNumbers.Push(0);
+            stackOfFibonacciNumbers.Push(1);
+
+            for (int i = 1; i < inputNumber; i++)
+            {
+
+                var lastNumber = stackOfFibonacciNumbers.Pop();
+                var previusNumber = stackOfFibonacciNumbers.Pop();
+
+                stackOfFibonacciNumbers.Push(lastNumber);
+                stackOfFibonacciNumbers.Push(lastNumber + previusNumber);
+
+            }
+
+            Console.WriteLine(stackOfFibonacciNumbers.Peek());
+        }
+    }
+}
