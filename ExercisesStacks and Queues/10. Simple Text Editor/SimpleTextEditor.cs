b63f401aa9e0081df07549c642aebc18590b4a9a
Stoil Valkov <valkovstoil@abv.bg>
2018-05-17 19:16:26 +0300

    work 100/100

--- ExercisesStacks and Queues/10. Simple Text Editor/SimpleTextEditor.cs
+++ ExercisesStacks and Queues/10. Simple Text Editor/SimpleTextEditor.cs
@@ -1,105 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace _10._Simple_Text_Editor
 {
     class SimpleTextEditor
     {
-        private static Stack<char> stack = new Stack<char>();
-        private static Stack<string> forUndo = new Stack<string>();
-
         static void Main(string[] args)
         {
             var n = int.Parse(Console.ReadLine());
+
+            var oldVersions = new Stack<string>();
+
+            var text = new StringBuilder();
 
             for (int i = 0; i < n; i++)
             {
                 var input = Console.ReadLine();
 
-                var opperations = input.Split();
+                var commandInput = input.Split();
 
-                var commands = int.Parse(opperations[0]);
+                var commands = int.Parse(commandInput[0]);
 
                 switch (commands)
                 {
                     case 1:
-                        foreach (var ch in opperations[1])
-                        {
-                            stack.Push(ch);
-                        }
-                        forUndo.Push(input);
+                        oldVersions.Push(text.ToString());
+                        string newText = commandInput[1];
+                        text.Append(newText);
                         break;
                     case 2:
-                        var charsToRemove = int.Parse(opperations[1]);
-
-                        Erases(charsToRemove);
+                        oldVersions.Push(text.ToString());
+                        var length = int.Parse(commandInput[1]);
+                        text.Remove(text.Length - length, length);
                         break;
                     case 3:
-                        var index = int.Parse(opperations[1]);
-
-                        Console.WriteLine(returnElementAtIndex(index));
+                        var index = int.Parse(commandInput[1]);
+                        Console.WriteLine(text[index - 1]);
                         break;
                     case 4:
-                        //TODO LOGIC FOR "UNDO"
-
-                        UNDO();
-
+                        text.Clear();
+                        text.Append(oldVersions.Pop());
                         break;
                 }
             }
-        }
-
-        private static void UNDO()
-        {
-            var lastCommand = forUndo.Pop().Split();
-
-            switch (lastCommand[0])
-            {
-                case "1":
-                    for (int i = 0; i < lastCommand[1].Length; i++)
-                    {
-                        stack.Pop();
-                    }
-                    break;
-                case "2":
-
-                    var addIn = lastCommand[1].ToCharArray().Reverse();
-
-                    foreach (var ch in addIn)
-                    {
-                        stack.Push(ch);
-                    }
-                    break;
-            }
-
-
-        }
-
-        public static char returnElementAtIndex(int index)
-        {
-            var findTheElementAtIndex = new Queue<char>(stack);
-
-            while (findTheElementAtIndex.Count > index)
-            {
-                findTheElementAtIndex.Dequeue();
-            }
-
-            return findTheElementAtIndex.Peek();
-        }
-
-        public static Stack<char> Erases(int opperations)
-        {
-            var rememberChars = string.Empty;
-
-            while (opperations > 0)
-            {
-                rememberChars += stack.Pop().ToString();
-                opperations--;
-            }
-
-            forUndo.Push($"2 {rememberChars}");
-            return stack;
         }
     }
 }
