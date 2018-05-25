b9d7f8f6ae25591d5e6f99b67ab439c60d80ae6e
Stoil Valkov <valkovstoil@abv.bg>
2018-05-25 11:21:43 +0300

    Sum Matrix Elements 1000/100 Square With Maximum Sum TODO

---
+++ Multidimensional Arrays - Lab/1. Sum Matrix Elements/SumMatrixElements.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    class SumMatrixElements
+    {
+        static void Main(string[] args)
+        {
+            var matrixDimensions = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.None)
+                .Select(int.Parse).ToArray();
+
+            var row = matrixDimensions[0];
+            var col = matrixDimensions[1];
+            var sum = 0;
+
+            int[,] matrix = new int[row, col];
+
+            for (int rows = 0; rows < row; rows++)
+            {
+                var matrixNumbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.None)
+                .Select(int.Parse).ToArray();
+
+                for (int columns = 0; columns < col; columns++)
+                {
+                    matrix[rows, columns] = matrixNumbers[columns];
+                    sum += matrixNumbers[columns];
+                }
+            }
+
+            Console.WriteLine(row);
+            Console.WriteLine(col);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Multidimensional Arrays - Lab/2. Square With Maximum Sum/SquareWithMaximumSum.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Square_With_Maximum_Sum
+{
+    class SquareWithMaximumSum
+    {
+        static void Main(string[] args)
+        {
+            var matrixDimensions = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.None)
+                .Select(int.Parse).ToArray();
+
+            var rows = matrixDimensions[0];
+            var columns = matrixDimensions[1];
+            var sum = int.MinValue;
+
+            var maxRowIndex = 0;
+            var maxColdIndex = 0;
+
+            int[,] matrix = new int[rows, columns];
+
+            for (int row = 0; row < rows; row++)
+            {
+                var elements = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.None)
+                .Select(int.Parse).ToArray();
+
+                for (int col = 0; col < columns; col++)
+                {
+                    matrix[row,col] = elements[col];
+                }
+            }
+
+            //TODO LOGIC for BiggestSubMatrix
+        }
+    }
+}
