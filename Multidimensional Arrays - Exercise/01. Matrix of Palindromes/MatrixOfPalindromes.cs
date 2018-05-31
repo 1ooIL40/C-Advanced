38930b5d059c9637ff8ee7741ba6de3f499f2081
Stoil Valkov <valkovstoil@abv.bg>
2018-05-31 12:40:26 +0300

    MatrixOfPalindromes work 100/100

---
+++ Multidimensional Arrays - Exercise/01. Matrix of Palindromes/MatrixOfPalindromes.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _01._Matrix_of_Palindromes
+{
+    class MatrixOfPalindromes
+    {
+        public static  char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+        static void Main(string[] args)
+        {
+            var rowsAndColumns = Console.ReadLine()
+                .Split(new string[] { " " }, StringSplitOptions.None)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            var rows = rowsAndColumns[0];
+            var columns = rowsAndColumns[1];
+
+            string[,] matrix = new string[rows, columns];
+
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = Polindromes(row, col);
+                }
+
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        public static string Polindromes(int row, int col)
+        {
+            var result = "";
+
+            result = alphabet[row].ToString() + alphabet[col + row].ToString() + alphabet[row].ToString();
+
+            return result;
+        }
+    }
+}
